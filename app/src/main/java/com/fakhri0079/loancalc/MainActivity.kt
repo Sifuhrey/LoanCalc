43db3dab10db49046c76170691c0a59592707244
Fakhri Abdillah <fakhriabdull03@gmail.com>
2025-04-06 22:39:43 +0700

    menambahkan top app bar

--- app/src/main/java/com/fakhri0079/loancalc/MainActivity.kt
+++ app/src/main/java/com/fakhri0079/loancalc/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.fakhri0079.loancalc
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.fakhri0079.loancalc.ui.theme.LoanCalcTheme
 
@@ -19,29 +24,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LoanCalcTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreenPreview() {
+    LoanCalcTheme {
+        MainScreen()
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { innerPadding ->
+       ScreenContent(Modifier.padding(innerPadding))
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LoanCalcTheme {
-        Greeting("Android")
-    }
+fun ScreenContent(modifier: Modifier = Modifier){
+    Text(
+        text = "Hello LoanCalc",
+        modifier = modifier
+    )
 }

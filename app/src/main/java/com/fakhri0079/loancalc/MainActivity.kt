8510747ebcb6d1e264a67a07f90e230619b69e1b
Fakhri Abdillah <fakhriabdull03@gmail.com>
2025-04-11 22:27:42 +0700

    membuat textfield dan button

--- app/src/main/java/com/fakhri0079/loancalc/MainActivity.kt
+++ app/src/main/java/com/fakhri0079/loancalc/MainActivity.kt
@@ -5,17 +5,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fakhri0079.loancalc.ui.theme.LoanCalcTheme
 
 class MainActivity : ComponentActivity() {
@@ -60,8 +75,74 @@ fun MainScreen(){
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier){
-    Text(
-        text = "Hello LoanCalc",
+    var pokok by remember { mutableStateOf("") }
+    var sukuBunga by remember { mutableStateOf("") }
+    var tenor by remember { mutableStateOf("") }
+    var totalWaktu by remember { mutableStateOf("") }
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.loan),
+            style = MaterialTheme.typography.bodyLarge
+        )
+        OutlinedTextField(
+            value = pokok,
+            onValueChange = { pokok = it },
+            label = { Text(text = stringResource(R.string.pokok)) },
+            trailingIcon = { Text(text = "Rp") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = sukuBunga,
+            onValueChange = { sukuBunga = it },
+            label = { Text(text = stringResource(R.string.suku_bunga)) },
+            trailingIcon = { Text(text = "%") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = tenor,
+            onValueChange = { tenor = it },
+            label = { Text(text = stringResource(R.string.tenor)) },
+            trailingIcon = { Text(text = "bulan") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = totalWaktu,
+            onValueChange = { totalWaktu = it },
+            label = { Text(text = stringResource(R.string.jumlah_bulan)) },
+            trailingIcon = { Text(text = "bulan") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = {},
+            modifier = Modifier.padding(top = 8.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ) {
+            Text(text = stringResource(R.string.hitung))
+        }
+    }
 }
